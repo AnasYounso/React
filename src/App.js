10760f5b6f6c0e240c51c9819904820eb0b84544
Anas Younso <anasyounso@gmail.com>
2018-10-12 12:50:41 +0200

    first upload React-Spiel

--- src/App.js
+++ src/App.js
@@ -1,28 +1,399 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import theimg from './logo.PNG';
 import './App.css';
 
-class App extends Component {
-  render() {
+
+ class App extends Component {
+  constructor( props ){
+   
+    super( props );
+    
+    this.state = {showFirst : true }
+    this.state = {show2 : true }
+    this.state = {show3 : true }
+    this.state = {show4 : true }
+    this.state = {show5 : true }
+    this.state = {show6 : true }
+    this.state = {show7 : true }
+    this.state = {show8 : true }
+    this.state = {show9 : true }
+    this.state = {showten : true }
+    this.state = {show11 : true }
+    this.state = {show12 : true }
+    this.state = {show13 : true }
+    this.state = {show14 : true }
+    this.state = {show15 : true }
+    this.state = {show16 : true }
+    this.state = {show17 : true }
+    this.state = {show18 : true }
+    this.state = {show19 : true }
+    this.state = {showtwen : true }
+    this.state = {show21 : true }
+    this.state = {show22 : true }
+    this.state = {show23 : true }
+    this.state = {show24 : true }
+    this.state = {show25 : true }
+    this.state = {show26 : true }
+    this.state = {show27 : true }
+    this.state = {show28 : true }
+    this.state = {show29 : true }
+    this.state = {show30 : true }
+    this.state = {show31 : true }
+    this.state = {show32 : true }
+    this.state = {show33 : true }
+    this.state = {show34 : true }
+    this.state = {show35 : true }
+    this.state = {show36 : true }
+    this.state = {show37 : true }
+    this.state = {show38 : true }
+    this.state = {show39 : true }
+    this.state = {show40 : true }
+    this.state = {show41 : true }
+    this.state = {show42 : true }
+    this.state = {show43 : true }
+    this.state = {show44 : true }
+    this.state = {show45 : true }
+    this.state = {show46 : true }
+    this.state = {show47 : true }
+    this.state = {show48 : true }
+    this.state = {show49 : true }
+    this.state = {show50 : true }
+  }
+
+  numFirstOperation(){
+    this.setState({showFirst:!this.state.showFirst})
+    }
+  
+    num2Operation(){
+    this.setState({show2:!this.state.show2})
+    }
+    
+    num3Operation(){
+    this.setState({show3:!this.state.show3})
+    }
+    
+    num4Operation(){
+    this.setState({show4:!this.state.show4})
+    }
+    
+    num5Operation(){
+    this.setState({show5:!this.state.show5})
+    }
+  
+    num6Operation(){
+    this.setState({show6:!this.state.show6})
+    }
+  
+    num7Operation(){
+    this.setState({show7:!this.state.show7})
+    }
+    
+    num8Operation(){
+    this.setState({show8:!this.state.show8})
+    }
+  
+    num9Operation(){
+    this.setState({show9:!this.state.show9})
+    }
+  
+    numtenOperation(){
+    this.setState({showten:!this.state.showten})
+    }
+     num11Operation(){
+    this.setState({show11:!this.state.show11})
+    }
+    
+    num12Operation(){
+    this.setState({show12:!this.state.show12})
+    }
+    num13Operation(){
+    this.setState({show13:!this.state.show13})
+    }
+    num14Operation(){
+    this.setState({show14:!this.state.show14})
+    }
+    num15Operation(){
+    this.setState({show15:!this.state.show15})
+    }
+    num16Operation(){
+    this.setState({show16:!this.state.show16})
+    }
+    num17Operation(){
+    this.setState({show17:!this.state.show17})
+    }
+    num18Operation(){
+    this.setState({show18:!this.state.show18})
+    }
+     
+     num19Operation(){
+    this.setState({show19:!this.state.show19})
+    }
+    numration(){
+    this.setState({showtwen:!this.state.showtwen})
+    }
+    
+    num21Operation(){
+    this.setState({show21:!this.state.show21})
+    }
+    
+    num22Operation(){
+    this.setState({show22:!this.state.show22})
+    }
+    num23Operation(){
+    this.setState({show23:!this.state.show23})
+    }
+    num24Operation(){
+    this.setState({show24:!this.state.show24})
+    }
+    num25Operation(){
+    this.setState({show25:!this.state.show25})
+    }
+    num26Operation(){
+    this.setState({show26:!this.state.show26})
+    }
+    num27Operation(){
+    this.setState({show27:!this.state.show27})
+    }
+    num28Operation(){
+    this.setState({show28:!this.state.show28})
+    }
+     
+     num29Operation(){
+    this.setState({show29:!this.state.show29})
+    }
+
+    num30Operation(){
+      this.setState({show30:!this.state.show30})
+      }
+      num31Operation(){
+      this.setState({show31:!this.state.show31})
+      }
+      num32Operation(){
+      this.setState({show32:!this.state.show32})
+      }
+      num33Operation(){
+      this.setState({show33:!this.state.show33})
+      }
+      num34Operation(){
+      this.setState({show34:!this.state.show34})
+      }
+      num35Operation(){
+      this.setState({show35:!this.state.show35})
+      }
+      num36Operation(){
+      this.setState({show36:!this.state.show36})
+      }
+       
+       num37Operation(){
+      this.setState({show37:!this.state.show37})
+      }
+
+      num38Operation(){
+        this.setState({show38:!this.state.show38})
+        }
+        num39Operation(){
+        this.setState({show39:!this.state.show39})
+        }
+        num40Operation(){
+        this.setState({show40:!this.state.show40})
+        }
+        num41Operation(){
+        this.setState({show41:!this.state.show41})
+        }
+        num42Operation(){
+        this.setState({show42:!this.state.show42})
+        }
+        num43Operation(){
+        this.setState({show43:!this.state.show43})
+        }
+        num44Operation(){
+        this.setState({show44:!this.state.show44})
+        }
+         
+         num45Operation(){
+        this.setState({show45:!this.state.show45})
+        }
+        num46Operation(){
+          this.setState({show46:!this.state.show46})
+          }
+          num47Operation(){
+          this.setState({show47:!this.state.show47})
+          }
+          num48Operation(){
+          this.setState({show48:!this.state.show48})
+          }
+           
+           num49Operation(){
+          this.setState({show49:!this.state.show49})
+          }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    
+
+    onlySevenNumber=()=>{
+    
+      var id1 =this.state.show1
+      var id2 =this.state.show2
+      var id3 =this.state.show3
+      var id4 =this.state.show4
+      var id5 =this.state.show5
+      var id6 =this.state.show6
+      var id7 =this.state.show7
+      var id8 =this.state.show8
+      var id9 =this.state.show9
+      var id10 =this.state.showten 
+      var id11 =this.state.show11 
+      var id12 =this.state.show12 
+      var id13 =this.state.show13 
+      var id14 =this.state.show14 
+      var id15 =this.state.show15 
+      var id16 =this.state.show16 
+      var id17 =this.state.show17 
+      var id18 =this.state.show18 
+      var id19 =this.state.show19 
+      var id20 =this.state.showtwen 
+      var id21 =this.state.show21 
+      var id22 =this.state.show22 
+      var id23 =this.state.show23 
+      var id24 =this.state.show24 
+      var id25 =this.state.show25 
+      var id26 =this.state.show26 
+      var id27 =this.state.show27 
+      var id28 =this.state.show28 
+      var id29 =this.state.show29 
+      var id30 =this.state.show30 
+      var id31 =this.state.show31 
+      var id32 =this.state.show32 
+      var id33 =this.state.show33 
+      var id34 =this.state.show34 
+      var id35 =this.state.show35 
+      var id36 =this.state.show36 
+      var id37 =this.state.show37 
+      var id38 =this.state.show38 
+      var id39 =this.state.show39 
+      var id40 =this.state.show40 
+      var id41 =this.state.show41 
+      var id42 =this.state.show42 
+      var id43 =this.state.show43 
+      var id44 =this.state.show44 
+      var id45 =this.state.show45 
+      var id46 =this.state.show46 
+      var id47 =this.state.show47 
+      var id48 =this.state.show48 
+      var id49 =this.state.show49
+      
+
+
+      var Array=[id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id13,id14,id15,id16,id17,id18,id19,id20,id21,id22,id23,id24,id25,id26,id27,id28,id29,id30,id31,id32,id33,id34,id35,id36,id37,id38,id39,id40,id41,id42,id43,id44,id45,id46,id47,id48,id49]
+      var trueSeven=[]
+       var sevenNumber = Array.find(function(elem){
+         if (elem==true) { trueSeven.push(elem); 
+          if (trueSeven.length==5){ document.getElementById("game").style.pointerEvents ="none"; 
+           document.getElementById("btn").style.display = ""
+          }
+         }
+         
+       })
+          
+    }
+     
+     
+
+  render(){
+      
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        
+        <div onClick={()=>this.onlySevenNumber()} id="game">
+       
+        <br/>
+        <img src={theimg}/>
+        <br/>
+        <div id="board" >
+        
+        <div className="square"   onClick={()=>this.numFirstOperation()} >{this.state.showFirst?  'X' :'1'}</div>
+        <div className="square"   onClick={()=>this.num2Operation()} ><div>{this.state.show2?  'X' :'2'}</div></div>
+        <div className="square"   onClick={()=>this.num3Operation()} ><div>{this.state.show3?  'X' :'3'}</div></div>
+        <div className="square"   onClick={()=>this.num4Operation()} ><div>{this.state.show4?  'X' :'4'}</div></div>
+        <div className="square"   onClick={()=>this.num5Operation()} ><div>{this.state.show5?  'X' :'5'}</div></div>
+        <div className="square"   onClick={()=>this.num6Operation()} ><div>{this.state.show6?  'X' :'6'}</div></div>
+        <div className="square"   onClick={()=>this.num7Operation()} ><div>{this.state.show7?  'X' :'7'}</div></div>
+        <div className="square"   onClick={()=>this.num8Operation()} ><div>{this.state.show8?  'X' :'8'}</div></div>
+        <div className="square"   onClick={()=>this.num9Operation()} ><div>{this.state.show9?  'X' :'9'}</div></div>
+        <div className="square"   onClick={()=>this.numtenOperation()} ><div>{this.state.showten?  'X' :'10'}</div></div>
+        <div className="square"   onClick={()=>this.num11Operation()} ><div>{this.state.show11?  'X' :'11'}</div></div>
+        <div className="square"   onClick={()=>this.num12Operation()} ><div>{this.state.show12?  'X' :'12'}</div></div>
+        <div className="square"   onClick={()=>this.num13Operation()} ><div>{this.state.show13?  'X' :'13'}</div></div>
+        <div className="square"   onClick={()=>this.num14Operation()} ><div>{this.state.show14?  'X' :'14'}</div></div>
+        <div className="square"   onClick={()=>this.num15Operation()} ><div>{this.state.show15?  'X' :'15'}</div></div>
+        <div className="square"   onClick={()=>this.num16Operation()} ><div>{this.state.show16?  'X' :'16'}</div></div>
+        <div className="square"   onClick={()=>this.num17Operation()} ><div>{this.state.show17?  'X' :'17'}</div></div>
+        <div className="square"   onClick={()=>this.num18Operation()} ><div>{this.state.show18?  'X' :'18'}</div></div>
+        <div className="square"   onClick={()=>this.num19Operation()} ><div>{this.state.show19?  'X' :'19'}</div></div>
+        <div className="square"   onClick={()=>this.numration()} ><div>{this.state.showtwen?  'X' :'20'}</div></div>
+        <div className="square"   onClick={()=>this.num21Operation()} ><div>{this.state.show21?  'X' :'21'}</div></div>
+        <div className="square"   onClick={()=>this.num22Operation()} ><div>{this.state.show22?  'X' :'22'}</div></div>
+        <div className="square"   onClick={()=>this.num23Operation()} ><div>{this.state.show23?  'X' :'23'}</div></div>
+        <div className="square"   onClick={()=>this.num24Operation()} ><div>{this.state.show24?  'X' :'24'}</div></div>
+        <div className="square"   onClick={()=>this.num25Operation()} ><div>{this.state.show25?  'X' :'25'}</div></div>
+        <div className="square"   onClick={()=>this.num26Operation()} ><div>{this.state.show26?  'X' :'26'}</div></div>
+        <div className="square"   onClick={()=>this.num27Operation()} ><div>{this.state.show27?  'X' :'27'}</div></div>
+        <div className="square"   onClick={()=>this.num28Operation()} ><div>{this.state.show28?  'X' :'28'}</div></div>
+        <div className="square"   onClick={()=>this.num29Operation()} ><div>{this.state.show29?  'X' :'29'}</div></div>
+        <div className="square"   onClick={()=>this.num30Operation()} ><div>{this.state.show30?  'X' :'30'}</div></div>
+        <div className="square"   onClick={()=>this.num31Operation()} ><div>{this.state.show31?  'X' :'31'}</div></div>
+        <div className="square"   onClick={()=>this.num32Operation()} ><div>{this.state.show32?  'X' :'32'}</div></div>
+        <div className="square"   onClick={()=>this.num33Operation()} ><div>{this.state.show33?  'X' :'33'}</div></div>
+        <div className="square"   onClick={()=>this.num34Operation()} ><div>{this.state.show34?  'X' :'34'}</div></div>
+        <div className="square"   onClick={()=>this.num35Operation()} ><div>{this.state.show35?  'X' :'35'}</div></div>
+        <div className="square"   onClick={()=>this.num36Operation()} ><div>{this.state.show36?  'X' :'36'}</div></div>
+        <div className="square"   onClick={()=>this.num37Operation()} ><div>{this.state.show37?  'X' :'37'}</div></div>
+        <div className="square"   onClick={()=>this.num38Operation()} ><div>{this.state.show38?  'X' :'38'}</div></div>
+        <div className="square"   onClick={()=>this.num39Operation()} ><div>{this.state.show39?  'X' :'39'}</div></div>
+        <div className="square"   onClick={()=>this.num40Operation()} ><div>{this.state.show40?  'X' :'40'}</div></div>
+        <div className="square"   onClick={()=>this.num41Operation()} ><div>{this.state.show41?  'X' :'41'}</div></div>
+        <div className="square"   onClick={()=>this.num42Operation()} ><div>{this.state.show42?  'X' :'42'}</div></div>
+        <div className="square"   onClick={()=>this.num43Operation()} ><div>{this.state.show43?  'X' :'43'}</div></div>
+        <div className="square"   onClick={()=>this.num44Operation()} ><div>{this.state.show44?  'X' :'44'}</div></div>
+        <div className="square"   onClick={()=>this.num45Operation()} ><div>{this.state.show45?  'X' :'45'}</div></div>
+        <div className="square"   onClick={()=>this.num46Operation()} ><div>{this.state.show46?  'X' :'46'}</div></div>
+        <div className="square"   onClick={()=>this.num47Operation()} ><div>{this.state.show47?  'X' :'47'}</div></div>
+        <div className="square"   onClick={()=>this.num48Operation()} ><div>{this.state.show48?  'X' :'48'}</div></div>
+        <div className="square"   onClick={()=>this.num49Operation()} ><div>{this.state.show49?  'X' :'49'}</div></div>
+        
+
+
+        </div>
+
+        <br/>
+        
+                <button style={{display : "none"}} className="button" id="btn"><span> Weiter </span></button>
+                
+       </div>
+    
     );
   }
 }
 
+
 export default App;
+
+
