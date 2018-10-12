35eb4bc06e7f17623b8d5d400a660de7af665b07
Anas Younso <anasyounso@gmail.com>
2018-10-12 14:35:42 +0200

    updated

---
+++ build/precache-manifest.2030ffb34a2a7b146f9c886fe3785a71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c4bb87f952910b0f8b971d8f97888d0",
+    "url": "/static/media/logo.4c4bb87f.PNG"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7e98872a3cb992b76f2d",
+    "url": "/static/js/main.7e98872a.chunk.js"
+  },
+  {
+    "revision": "cb79bc868f60eae48214",
+    "url": "/static/js/1.cb79bc86.chunk.js"
+  },
+  {
+    "revision": "7e98872a3cb992b76f2d",
+    "url": "/static/css/main.24f12e6f.chunk.css"
+  },
+  {
+    "revision": "cb79bc868f60eae48214",
+    "url": "/static/css/1.9e265b4c.chunk.css"
+  },
+  {
+    "revision": "6417590e43bd1dce90b0e9d557275310",
+    "url": "/index.html"
+  }
+];
