4aa9a790a5538d65eb8969b60ec102438355887a
Anas Younso <anas_younso@hotmail.com>
2018-10-13 19:34:03 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
    
     super( props );
     this.state = {ontoggle : false}
-    this.state = {showFirst : true }
+    this.state = {show1 : true }
     this.state = {show2 : true }
     this.state = {show3 : true }
     this.state = {show4 : true }
@@ -17,7 +17,7 @@ import './App.css';
     this.state = {show7 : true }
     this.state = {show8 : true }
     this.state = {show9 : true }
-    this.state = {showten : true }
+    this.state = {show10 : true }
     this.state = {show11 : true }
     this.state = {show12 : true }
     this.state = {show13 : true }
@@ -27,7 +27,7 @@ import './App.css';
     this.state = {show17 : true }
     this.state = {show18 : true }
     this.state = {show19 : true }
-    this.state = {showtwen : true }
+    this.state = {show20 : true }
     this.state = {show21 : true }
     this.state = {show22 : true }
     this.state = {show23 : true }
@@ -60,14 +60,14 @@ import './App.css';
     this.state = {show50 : true }
   }
    
-  toggle = () => {
+  	toggle = () => {
     this.setState({ontoggle :!this.state.ontoggle})
-  }
+ 	}
 
-  numFirstOperation(){
-    this.setState({showFirst:!this.state.showFirst})
+ 	num1Operation(){
+    this.setState({show1:!this.state.show1})
     }
-  
+    
     num2Operation(){
     this.setState({show2:!this.state.show2})
     }
@@ -100,8 +100,8 @@ import './App.css';
     this.setState({show9:!this.state.show9})
     }
   
-    numtenOperation(){
-    this.setState({showten:!this.state.showten})
+    num10Operation(){
+    this.setState({show10:!this.state.show10})
     }
      num11Operation(){
     this.setState({show11:!this.state.show11})
@@ -110,30 +110,36 @@ import './App.css';
     num12Operation(){
     this.setState({show12:!this.state.show12})
     }
+
     num13Operation(){
     this.setState({show13:!this.state.show13})
     }
+
     num14Operation(){
     this.setState({show14:!this.state.show14})
     }
+
     num15Operation(){
     this.setState({show15:!this.state.show15})
     }
+
     num16Operation(){
     this.setState({show16:!this.state.show16})
     }
+
     num17Operation(){
     this.setState({show17:!this.state.show17})
     }
+
     num18Operation(){
     this.setState({show18:!this.state.show18})
     }
      
-     num19Operation(){
+    num19Operation(){
     this.setState({show19:!this.state.show19})
     }
-    numration(){
-    this.setState({showtwen:!this.state.showtwen})
+    num20Operation(){
+    this.setState({show20:!this.state.show20})
     }
     
     num21Operation(){
@@ -162,99 +168,94 @@ import './App.css';
     this.setState({show28:!this.state.show28})
     }
      
-     num29Operation(){
+    num29Operation(){
     this.setState({show29:!this.state.show29})
     }
 
     num30Operation(){
-      this.setState({show30:!this.state.show30})
-      }
-      num31Operation(){
-      this.setState({show31:!this.state.show31})
-      }
-      num32Operation(){
-      this.setState({show32:!this.state.show32})
-      }
-      num33Operation(){
-      this.setState({show33:!this.state.show33})
-      }
-      num34Operation(){
-      this.setState({show34:!this.state.show34})
-      }
-      num35Operation(){
-      this.setState({show35:!this.state.show35})
-      }
-      num36Operation(){
-      this.setState({show36:!this.state.show36})
-      }
+    this.setState({show30:!this.state.show30})
+    }
+
+    num31Operation(){
+    this.setState({show31:!this.state.show31})
+    }
+
+    num32Operation(){
+    this.setState({show32:!this.state.show32})
+    }
+
+    num33Operation(){
+    this.setState({show33:!this.state.show33})
+    }
+
+    num34Operation(){
+    this.setState({show34:!this.state.show34})
+    }
+
+    num35Operation(){
+    this.setState({show35:!this.state.show35})
+    }
+
+    num36Operation(){
+    this.setState({show36:!this.state.show36})
+    }
        
-       num37Operation(){
-      this.setState({show37:!this.state.show37})
-      }
+    num37Operation(){
+    this.setState({show37:!this.state.show37})
+    }
 
-      num38Operation(){
-        this.setState({show38:!this.state.show38})
-        }
-        num39Operation(){
-        this.setState({show39:!this.state.show39})
-        }
-        num40Operation(){
-        this.setState({show40:!this.state.show40})
-        }
-        num41Operation(){
-        this.setState({show41:!this.state.show41})
-        }
-        num42Operation(){
-        this.setState({show42:!this.state.show42})
-        }
-        num43Operation(){
-        this.setState({show43:!this.state.show43})
-        }
-        num44Operation(){
-        this.setState({show44:!this.state.show44})
-        }
-         
-         num45Operation(){
-        this.setState({show45:!this.state.show45})
-        }
-        num46Operation(){
-          this.setState({show46:!this.state.show46})
-          }
-          num47Operation(){
-          this.setState({show47:!this.state.show47})
-          }
-          num48Operation(){
-          this.setState({show48:!this.state.show48})
-          }
+    num38Operation(){
+    this.setState({show38:!this.state.show38})
+    }
+
+    num39Operation(){
+    this.setState({show39:!this.state.show39})
+    }
+
+    num40Operation(){
+    this.setState({show40:!this.state.show40})
+    }
+
+    num41Operation(){
+    this.setState({show41:!this.state.show41})
+    }
+
+    num42Operation(){
+    this.setState({show42:!this.state.show42})
+    }
+
+    num43Operation(){
+    this.setState({show43:!this.state.show43})
+    }
+
+    num44Operation(){
+    this.setState({show44:!this.state.show44})
+    }
+        
+    num45Operation(){
+    this.setState({show45:!this.state.show45})
+    }
+
+    num46Operation(){
+    this.setState({show46:!this.state.show46})
+    }
+
+    num47Operation(){
+    this.setState({show47:!this.state.show47})
+    }
+
+    num48Operation(){
+    this.setState({show48:!this.state.show48})
+    }
            
-           num49Operation(){
-          this.setState({show49:!this.state.show49})
-          }
+    num49Operation(){
+    this.setState({show49:!this.state.show49})
+    }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-    
-
-    onlySevenNumber=()=>{
-    
+    onlySevenNumber=()=>{  
       var id1 =this.state.show1
       var id2 =this.state.show2
       var id3 =this.state.show3
@@ -264,7 +265,7 @@ import './App.css';
       var id7 =this.state.show7
       var id8 =this.state.show8
       var id9 =this.state.show9
-      var id10 =this.state.showten 
+      var id10 =this.state.show10 
       var id11 =this.state.show11 
       var id12 =this.state.show12 
       var id13 =this.state.show13 
@@ -274,7 +275,7 @@ import './App.css';
       var id17 =this.state.show17 
       var id18 =this.state.show18 
       var id19 =this.state.show19 
-      var id20 =this.state.showtwen 
+      var id20 =this.state.show20
       var id21 =this.state.show21 
       var id22 =this.state.show22 
       var id23 =this.state.show23 
@@ -304,20 +305,20 @@ import './App.css';
       var id47 =this.state.show47 
       var id48 =this.state.show48 
       var id49 =this.state.show49
-      
 
-
-      var Array=[id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id13,id14,id15,id16,id17,id18,id19,id20,id21,id22,id23,id24,id25,id26,id27,id28,id29,id30,id31,id32,id33,id34,id35,id36,id37,id38,id39,id40,id41,id42,id43,id44,id45,id46,id47,id48,id49]
+      var Array=[id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,
+      id11,id12,id13,id14,id15,id16,id17,id18,id19,id20,
+      id21,id22,id23,id24,id25,id26,id27,id28,id29,id30,
+      id31,id32,id33,id34,id35,id36,id37,id38,id39,id40,
+      id41,id42,id43,id44,id45,id46,id47,id48,id49]
       var trueSeven=[]
        var sevenNumber = Array.find(function(elem){
-         if (elem==true) { trueSeven.push(elem); 
-          if (trueSeven.length==5){ document.getElementById("board").style.pointerEvents ="none"; 
-           document.getElementById("btn").style.display = ""
-          }
-         }
-         
-       })
-          
+         		if (elem==true) { trueSeven.push(elem); 
+          							if (trueSeven.length==5)
+          						{ document.getElementById("board").style.pointerEvents ="none"; 
+           					      document.getElementById("btn").style.display = "" }          
+        						 }         
+       							})          
     }
      
      
@@ -327,15 +328,13 @@ import './App.css';
     return (
         
         <div onClick={()=>this.onlySevenNumber()} id="game">
-       
         <br/>
         <img src={theimg}/>
-        <div>{this.state.ontoggle && (<h1>Kostet 1 Euro Gehe zur Lotto Seite <small><a href="https://www.lotto.de/lotto-6aus49/spielen" >Klick hier</a></small> </h1>)}</div>
+        <div>{this.state.ontoggle && (<h1>Kostet 1 Euro. Bitte gehe zur Lotto-Seite <small><a href="https://www.lotto.de/lotto-6aus49/spielen" >Klick hier</a></small> </h1>)}</div>
         
         <br/>
-        <div id="board" >
-        
-        <div className="square"   onClick={()=>this.numFirstOperation()} >{this.state.showFirst?  'X' :'1'}</div>
+        <div id="board" >        
+        <div className="square"   onClick={()=>this.num1Operation()} ><div>{this.state.show1?  'X' :'1'}</div></div>
         <div className="square"   onClick={()=>this.num2Operation()} ><div>{this.state.show2?  'X' :'2'}</div></div>
         <div className="square"   onClick={()=>this.num3Operation()} ><div>{this.state.show3?  'X' :'3'}</div></div>
         <div className="square"   onClick={()=>this.num4Operation()} ><div>{this.state.show4?  'X' :'4'}</div></div>
@@ -344,7 +343,7 @@ import './App.css';
         <div className="square"   onClick={()=>this.num7Operation()} ><div>{this.state.show7?  'X' :'7'}</div></div>
         <div className="square"   onClick={()=>this.num8Operation()} ><div>{this.state.show8?  'X' :'8'}</div></div>
         <div className="square"   onClick={()=>this.num9Operation()} ><div>{this.state.show9?  'X' :'9'}</div></div>
-        <div className="square"   onClick={()=>this.numtenOperation()} ><div>{this.state.showten?  'X' :'10'}</div></div>
+        <div className="square"   onClick={()=>this.num10Operation()} ><div>{this.state.show10?  'X' :'10'}</div></div>
         <div className="square"   onClick={()=>this.num11Operation()} ><div>{this.state.show11?  'X' :'11'}</div></div>
         <div className="square"   onClick={()=>this.num12Operation()} ><div>{this.state.show12?  'X' :'12'}</div></div>
         <div className="square"   onClick={()=>this.num13Operation()} ><div>{this.state.show13?  'X' :'13'}</div></div>
@@ -354,7 +353,7 @@ import './App.css';
         <div className="square"   onClick={()=>this.num17Operation()} ><div>{this.state.show17?  'X' :'17'}</div></div>
         <div className="square"   onClick={()=>this.num18Operation()} ><div>{this.state.show18?  'X' :'18'}</div></div>
         <div className="square"   onClick={()=>this.num19Operation()} ><div>{this.state.show19?  'X' :'19'}</div></div>
-        <div className="square"   onClick={()=>this.numration()} ><div>{this.state.showtwen?  'X' :'20'}</div></div>
+        <div className="square"   onClick={()=>this.num20Operation()} ><div>{this.state.show20?  'X' :'20'}</div></div>
         <div className="square"   onClick={()=>this.num21Operation()} ><div>{this.state.show21?  'X' :'21'}</div></div>
         <div className="square"   onClick={()=>this.num22Operation()} ><div>{this.state.show22?  'X' :'22'}</div></div>
         <div className="square"   onClick={()=>this.num23Operation()} ><div>{this.state.show23?  'X' :'23'}</div></div>
@@ -384,17 +383,12 @@ import './App.css';
         <div className="square"   onClick={()=>this.num47Operation()} ><div>{this.state.show47?  'X' :'47'}</div></div>
         <div className="square"   onClick={()=>this.num48Operation()} ><div>{this.state.show48?  'X' :'48'}</div></div>
         <div className="square"   onClick={()=>this.num49Operation()} ><div>{this.state.show49?  'X' :'49'}</div></div>
-        
 
-
-        </div>
-               
+        </div>               
         <br/>
-                       <div onClick={this.toggle} >   
-                <button  style={{display : "none"}} className="button" id="btn" href="https://www.lotto.de/">Weiter</button></div> 
-                
-       </div>
-    
+        <div onClick={this.toggle} >   
+        <button  style={{display : "none"}} className="button" id="btn" href="https://www.lotto.de/">Weiter</button></div>    
+        </div>    
     );
   }
 }
